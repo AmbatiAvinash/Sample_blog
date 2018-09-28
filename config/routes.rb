2287a9c1aff6c34bf2a4141f7d8cdfc92f5fce3e
AmbatiAvinash <avi.ambati07@gmail.com>
2018-09-28 15:19:22 +0530

    basic CRUD operations done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+
+  # get 'articles/new'
+
+  # get 'articles/edit'
+
+  # get 'articles/show'
+
+  # get 'welcome/index'
+
+
+  resources :articles
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
